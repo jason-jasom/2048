9abb38c495c10da66d23b200d2baf2813d5280b2
jason-jasom <78861576+jason-jasom@users.noreply.github.com>
2021-06-12 12:29:55 +0800

    Add files via upload

---
+++ header.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define N 4
+
+int map[N][N];
+
+void start(void);
+
+void random_start(void);
+
+void map_print(void);
+
+int up(void);
+
+int down(void);
+
+int right(void);
+
+int left(void);
+
+int check(void);
